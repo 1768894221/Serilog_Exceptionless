63b53ea23ad563f61673fe1ae67a0bd5dab4a9af
1768894221 <1768894221@qq.com>
2021-12-14 18:01:47 +0800

    Serilog_Exceptionless

---
+++ 01_Logging/Program.cs
@@ -0,0 +1,43 @@
+﻿using Exceptionless;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
+using System;
+
+
+
+namespace _01_Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///模拟startup实例一个ServiceCollection 
+            ServiceCollection services = new ServiceCollection();
+            ExceptionlessClient.Default.Startup("gF0rVHtu0IZrXM0LmVhZpWIreFJOWV3OZoUssW69");
+            services.AddLogging(
+                logBuilder=>
+                {
+                    Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(new JsonFormatter())
+                    .WriteTo.Exceptionless()
+                    .CreateLogger();
+                    logBuilder.AddSerilog();
+                }
+                );
+            //注册服务
+            services.AddScoped<SystemService>();
+            //通过服务构建容器，ServiceProvider类型
+            using (var sp = services.BuildServiceProvider())
+            { ///获取容器中的服务ITestService
+                var test2 = sp.GetRequiredService<SystemService>();
+                    test2.Logging();
+              
+            }
+        }
+    }
+}
---
+++ 01_Logging/SystemService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _01_Logging
+{
+    public class SystemService
+    {
+        private ILogger<SystemService> logger;
+        public SystemService(ILogger<SystemService> logger)
+        {
+            this.logger = logger;
+        }
+        public void Logging()
+        {
+            logger.LogDebug("开始执行数据库同步");
+            logger.LogDebug("开始执行数据库同成功");
+            logger.LogDebug("connection succed");
+            logger.LogWarning("read data faile, retry first");
+            //......
+            User person = new User { Id="1",Name="lty"};
+            logger.LogWarning("read data faile, retry second");
+            logger.LogError("read data faile");
+            //要记录的结构化数据通过占位符来输出；
+            logger.LogDebug("注册一个用户{@person}", person);
+        
+        }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
